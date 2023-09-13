d79513c4944fc3fecc8e763cf3e53a65d1c170cf
CarlosZ360 <carlos.zarate@ucb.edu.bo>
2023-09-13 18:20:21 -0400

    tap2 structure preview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Tap2Component } from './components/initial-config/tap2/tap2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tap2', component: Tap2Component }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
