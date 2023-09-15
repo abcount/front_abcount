4542514532a5b67639b3570a33d75ccb0f286b7d
AndrePxd <andrepanique@gmail.com>
2023-09-15 00:16:50 -0400

    Agregando ruta tap3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { Tap2Component } from './components/initial-config/tap2/tap2.component';
 import {Tap4Component} from "./components/initial-config/tap4/tap4.component";
 import {Tap1Component} from "./components/initial-config/tap1/tap1.component";
+import {Tap3Component} from "./components/initial-config/tap3/tap3.component";
 
 const routes: Routes = [
   {path: 'tap1', component: Tap1Component}, // {4}
   { path: 'tap2', component: Tap2Component },
+  { path: 'tap3', component: Tap3Component },
   { path: 'tap4', component: Tap4Component }
 
 
