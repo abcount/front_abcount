2205a6e442e54a51e81bfdcb04d5037ea947dcda
MicaelaGordillo <micaa.gordilloa@gmail.com>
2023-09-20 18:26:52 -0400

    Styles tap 2

--- src/app/components/initial-config/tap2/tap2.component.ts
+++ src/app/components/initial-config/tap2/tap2.component.ts
@@ -15,109 +15,98 @@ interface Elemento {
 })
 export class Tap2Component {
 
+  /*Input nombre se sucursal*/
   iconInputNombre = "fa-solid fa-shop";
-  iconInputArea = "fa-regular fa-building";
   labelNombre = 'Nombre de la sucursal';
-  labelDireccion = 'Dirección';
-  labelNombreArea = 'Nombre del area';
-  placeholderDireccion = 'Ingrese la dirección de la sucursal';
-  iconInputDireccion = "fa-regular fa-map-marker-alt";
-  buttonText: string = 'AGREGAR';
   placeholderSubsidiary: string = 'Ingrese el nombre de tu sucursal';
-
+  /*Input dirección sucursal*/
+  iconInputDireccion = "fa-regular fa-map-marker-alt";
+  labelDireccion = 'Dirección de la sucursal';
   placeholderAddress: string = 'Ingrese la dirección de tu sucursal';
+  /*Input area*/
+  iconInputArea = "fa-regular fa-building";
+  labelNombreArea = 'Nombre del area';
   placeholderArea: string = 'Ingrese el nombre de tu área';
+
+  /*Patrones generales*/
+  patternAll = '.*';
+  patternAllMessage = 'Ingrese un valor válido';
+
+  /*Controladores*/
+  controlSubsidiaryName = new FormControl();
+  controlSubsidiaryAddress = new FormControl();
+  controlAreaName = new FormControl();
+
+  //Constructor
+  constructor(private formStateService: FormStateService) { }
   
   // Una de las siguientes estructuras para guardar los datos
-
   sucursal: { nombre: string, area: [{nombre: string}] }[] = [];
   empresa:{nombre: string, sucursal: [{nombre: string, area: [{nombre: string}]}]}[] = [];
 
-  arbol: Elemento[] = [
+  sucursales: any[] = [
     {
-      nombre: 'Empresa textilon',
-      children: [
-        {
-          nombre: 'Sucursal centro',
-          children: [
-            {
-              nombre: 'Ventas',
-              children: []
-            },
-            {
-              nombre: 'Finanzas',
-              children: []
-            },
-            {
-              nombre: 'Control',
-              children: []
-            }
-          ]
-        },
-        {
-          nombre: 'Sucursal sur',
-          children: [
-            {
-              nombre: 'Ventas',
-              children: []
-            }
-          ]
-        }
-      ]
+      subsidiaryName: 'Sucursal centro',
+      subsidiaryDirection: 'Centro La Paz'
+    },
+    {
+      subsidiaryName: 'Sucursal sur',
+      subsidiaryDirection: 'Sur La Paz'
     }
   ];
-  // la logica para agregar
+  areas: any[] = ['Ventas', 'Finanza', 'Contabilidad'];
+  
+  //Lógica para agregar
   agregarSucursal() {
-    console.log("agregar sucursal");
-    this.arbol.push({
-      nombre: this.placeholderSubsidiary,
-      children: [
-        {
-          nombre: 'Ventas',
-          children: []
-        },
-        {
-          nombre: 'Finanzas',
-          children: []
-        },
-      ]
-
-      
+    console.log("Agregar sucursal");
+    this.sucursales.push({
+      subsidiaryName: this.controlSubsidiaryName.value,
+      subsidiaryDirection: this.controlSubsidiaryAddress.value
     });
-    console.log(this.arbol); 
+    console.log(this.sucursales);
+    this.limpiarCampos();
   }
-  // la logica para eliminar
-  eliminarSucursal() {
-    console.log("eliminar sucursal");
-    this.arbol.pop();
-    console.log(this.arbol);
+  //Lógica para eliminar
+  eliminarSucursal(sucursalName: string) {
+    console.log("Eliminar sucursal");
+    this.sucursales = this.sucursales.filter((sucursal) => sucursal.subsidiaryName !== sucursalName);
+    console.log(this.sucursales);
   }
+
+  //Logica para agregar area
+  agregarArea() {
+    console.log('Agregar área');
+    this.areas.push(this.controlAreaName.value);
+    console.log(this.areas);
+    this.limpiarCampos();
+  }
+  //Logica para eliminar area
+  eliminarArea(areaName: string) {
+    console.log('Eliminar área');
+    this.areas = this.areas.filter((area) => area !== areaName);
+    console.log(this.areas);
+  }
+
+  //Limpiar campos
+  limpiarCampos() {
+    this.controlSubsidiaryName.reset();
+    this.controlSubsidiaryAddress.reset();
+    this.controlAreaName.reset();
+  }
+
+  //Lógica para mostrar y ocultar hijos
   toggleChildren(elemento: Elemento): void {
     elemento.mostrarHijos = !elemento.mostrarHijos;
   }
 
-
-  onClick() {
-    console.log('Button clicked!');
-  }
-
-  onInputText(text: string) {
-    console.log('Text changed: ' + text);
-  }
-
-  constructor(private formStateService: FormStateService) {
-    
-  }
+  //Imprimiendo datos
   printValue() {
     console.log(JSON.stringify(this.formStateService.getForm().value, null, 2));
   }
-
   get formGroup(): FormGroup {
     return this.formStateService.form;
   }
   get subsidiaryControl(): FormControl {
     return this.formGroup.get('subsidiary') as FormControl;
   }
-
-
 }
