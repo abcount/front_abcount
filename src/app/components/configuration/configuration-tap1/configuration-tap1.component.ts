bb0439e93ee0e44e21d13d8e868e8c370d3b79e2
MicaelaGordillo <micaa.gordilloa@gmail.com>
2023-09-24 00:05:54 -0400

    Creation of config-navbar and configuration tap 1 css and functionality

--- src/app/components/configuration/configuration-tap1/configuration-tap1.component.ts
+++ src/app/components/configuration/configuration-tap1/configuration-tap1.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-configuration-tap1',
@@ -14,4 +14,64 @@ export class ConfigurationTap1Component {
   placeholderEmail: string = 'Ingrese el email de tu sucursal';
   placeholderNumeroContacto: string = 'Ingrese el número de contacto de tu sucursal';
 
+  empresa = {
+    nombre: 'Empresa textil',
+    direccion: 'Av. 6 de Agosto',	
+    rubro: 'Textil',
+    nit: '123456789',
+    email: 'emp.textil@gmail.com',
+    numeroContacto: '12345678',
+    logo: '../../../../assets/imagen.svg',
+  }
+
+  @ViewChild('nombreInput') nombreInput: any;
+  @ViewChild('rubroInput') rubroInput: any;
+  @ViewChild('nitInput') nitInput: any;
+  srcLogo: string = '../../../../assets/imagen.svg';
+  @ViewChild('direccionInput') direccionInput: any;
+  @ViewChild('emailInput') emailInput: any;
+  @ViewChild('numeroContactoInput') numeroContactoInput: any;
+
+  modoEdicion: boolean = false;
+
+  // Cargar los datos a los inputs al iniciar la pantalla
+  ngAfterViewInit (){
+    this.nombreInput.nativeElement.value = this.empresa.nombre;
+    this.rubroInput.nativeElement.value = this.empresa.rubro;
+    this.nitInput.nativeElement.value = this.empresa.nit;
+    this.srcLogo = this.empresa.logo;
+    this.direccionInput.nativeElement.value = this.empresa.direccion;
+    this.emailInput.nativeElement.value = this.empresa.email;
+    this.numeroContactoInput.nativeElement.value = this.empresa.numeroContacto;
+    //Deshabilitar los inputs
+    this.habilitar();
+  }
+
+  // Habilitar los inputs para editar
+  editar () {
+    this.modoEdicion = true;
+    this.habilitar();
+  }
+
+  // Guardar cambios
+  guardar () {
+    this.modoEdicion = false;
+    this.habilitar();
+    this.empresa.nombre = this.nombreInput.nativeElement.value;
+    this.empresa.rubro = this.rubroInput.nativeElement.value;
+    this.empresa.nit = this.nitInput.nativeElement.value;
+    this.empresa.direccion = this.direccionInput.nativeElement.value;
+    this.empresa.email = this.emailInput.nativeElement.value;
+    this.empresa.numeroContacto = this.numeroContactoInput.nativeElement.value;
+  }
+
+  // Habilitar campos inputs
+  habilitar () {
+    this.nombreInput.nativeElement.disabled = !this.modoEdicion;
+    this.rubroInput.nativeElement.disabled = !this.modoEdicion;
+    this.nitInput.nativeElement.disabled = !this.modoEdicion;
+    this.direccionInput.nativeElement.disabled = !this.modoEdicion;
+    this.emailInput.nativeElement.disabled = !this.modoEdicion;
+    this.numeroContactoInput.nativeElement.disabled = !this.modoEdicion;
+  }
 }
--- src/app/modules/CarlosModule.ts
+++ src/app/modules/CarlosModule.ts
@@ -1,8 +1,10 @@
 import {NgModule} from "@angular/core";
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { ConfigurationTap1Component } from '../components/configuration/configuration-tap1/configuration-tap1.component';
-
-
+import { ConfigNavbarComponent } from "../components/configuration/config-navbar/config-navbar.component";
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
+import { SebastianModule } from "./SebastianModule";
 
 const CarlosComponents = [
     NavbarComponent,
@@ -12,12 +14,15 @@ const CarlosComponents = [
 @NgModule({
     declarations: [
         NavbarComponent,
-        ConfigurationTap1Component
-
-    ],
-    imports: [
-        
+        ConfigurationTap1Component,
+        ConfigNavbarComponent,
     ],
     exports: [CarlosComponents] // Poner [CarlosComponents]
+    ,
+    imports: [
+        CommonModule,
+        AppRoutingModule,
+        SebastianModule
+    ]
 })
 export class CarlosModule {}
