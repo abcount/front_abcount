3b1bbaf623be01825ba8936351f77f442982ddb6
MicaelaGordillo <micaa.gordilloa@gmail.com>
2023-10-03 20:44:51 -0400

    Adjusting sizes in Add user permissions

--- src/app/components/configuration/add-users-and-permissions/add-users-and-permissions.component.ts
+++ src/app/components/configuration/add-users-and-permissions/add-users-and-permissions.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-users-and-permissions',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AddUsersAndPermissionsComponent {
 
+  constructor(private router: Router) { }
+
+  cancel(){
+    this.router.navigate(['/configuration-tap/5']);
+  }
 }
