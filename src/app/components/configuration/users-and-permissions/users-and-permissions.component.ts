4b0d38235de16b184884045061eb11d2d86bb4e0
CarlosZ360 <carlos.zarate@ucb.edu.bo>
2023-10-04 17:24:48 -0400

    details tree in administration user permissions

--- src/app/components/configuration/users-and-permissions/users-and-permissions.component.ts
+++ src/app/components/configuration/users-and-permissions/users-and-permissions.component.ts
@@ -7,6 +7,38 @@ import { Router } from '@angular/router';
   styleUrls: ['./users-and-permissions.component.css']
 })
 export class UsersAndPermissionsComponent {
+  //Actualizar para el Json
+  subsidiaries = [
+    {
+      subsidiaryId: 1,
+      subsidiaryName: "Sucursal 1",
+      address: "Av. 6 de Agosto",
+      editable: false
+    },
+    {
+      subsidiaryId: 2,
+      subsidiaryName: "Sucursal 2",
+      address: "Av. 6 de Agosto",
+      editable: true
+    }
+  ];
+  areas =[
+    {
+      areaId: 1,
+      areaName: "Area 1",
+      editable: false
+    },
+    {
+      areaId: 2,
+      areaName: "Area 2",
+      editable: true
+    },
+    {
+      areaId: 3,
+      areaName: "Area 3",
+      editable: false
+    }
+  ];
 
   user = {
     "userId": 5,
