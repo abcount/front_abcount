8dd34ce1cd362fa7e405540b503996258697d346
MicaelaGordillo <micaa.gordilloa@gmail.com>
2023-10-03 20:06:45 -0400

    Adjusting sizes in Configuration tap 5

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,10 +11,14 @@ import { Observable } from 'rxjs';
 export class UserService {
 
   constructor(private http: HttpClient) { }
+
   configurationUrl = `${environment.BACKEND_URL}/config/enterprise`;
+
+  companyId = localStorage.getItem('companyId');
+
   // Función para obtener usuarios de la empresa
   getUsers() {
-    return this.http.get(`${this.configurationUrl}/invitation/pending`);
+    return this.http.get(`${this.configurationUrl}/invitation/pending/${this.companyId}`);
   }
   // Función para eliminar usuarios de la empresa
   deleteUser(id: any) {
