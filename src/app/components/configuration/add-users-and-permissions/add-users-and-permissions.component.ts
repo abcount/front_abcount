3028539c4cdc277ff7d40620db31288dc6b188c1
CarlosZ360 <carlos.zarate@ucb.edu.bo>
2023-10-04 17:21:43 -0400

    details tree in user permissions

--- src/app/components/configuration/add-users-and-permissions/add-users-and-permissions.component.ts
+++ src/app/components/configuration/add-users-and-permissions/add-users-and-permissions.component.ts
@@ -9,6 +9,39 @@ import { Router } from '@angular/router';
 export class AddUsersAndPermissionsComponent {
 
   constructor(private router: Router) { }
+  
+  //Actualizar para el Json
+  subsidiaries = [
+    {
+      subsidiaryId: 1,
+      subsidiaryName: "Sucursal 1",
+      address: "Av. 6 de Agosto",
+      editable: false
+    },
+    {
+      subsidiaryId: 2,
+      subsidiaryName: "Sucursal 2",
+      address: "Av. 6 de Agosto",
+      editable: true
+    }
+  ];
+  areas =[
+    {
+      areaId: 1,
+      areaName: "Area 1",
+      editable: false
+    },
+    {
+      areaId: 2,
+      areaName: "Area 2",
+      editable: true
+    },
+    {
+      areaId: 3,
+      areaName: "Area 3",
+      editable: false
+    }
+  ];
 
   cancel(){
     this.router.navigate(['/configuration-tap/5']);
