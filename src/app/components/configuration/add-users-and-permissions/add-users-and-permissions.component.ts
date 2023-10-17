0e1bf319b88d4258c226a2c63412f282df815290
CarlosZ360 <carlos.zarate@ucb.edu.bo>
2023-10-17 14:07:43 -0400

    fixed user-and-permissos

--- src/app/components/configuration/add-users-and-permissions/add-users-and-permissions.component.ts
+++ src/app/components/configuration/add-users-and-permissions/add-users-and-permissions.component.ts
@@ -18,52 +18,118 @@ export class AddUsersAndPermissionsComponent {
   sucursalesSeleccionadas: number[] = [];
   rolesSeleccionados: number[] = [];
   idsRolesSeleccionados: number[] = [];
+  resultados: any[] = [];
 
-  //Actualizar para el Json
+  //Json para obtener las sucursales, areas y roles
   subsidiaries = [
     {
       subsidiaryId: 1,
       subsidiaryName: "Sucursal 1",
       address: "Av. 6 de Agosto",
-      selected: false
+      status: false
     },
     {
       subsidiaryId: 2,
       subsidiaryName: "Sucursal 2",
       address: "Av. 6 de Agosto",
-      selected: false
+      status: false
     }
   ];
   areas =[
     {
       areaId: 1,
       areaName: "Area 1",
-      selected: false
+      status: false
     },
     {
       areaId: 2,
       areaName: "Area 2",
-      selected: false
+      status: false
     },
     {
       areaId: 3,
       areaName: "Area 3",
-      selected: false
+      status: false
     }
   ];
 
   roles = [
-    { id: 1, nombre: 'Agregar comprobantes', seleccionado: false },
-    { id: 2, nombre: 'Editar comprobantes', seleccionado: false },
-    { id: 3, nombre: 'Generar reportes', seleccionado: false },
-    { id: 4, nombre: 'Agregar tasa de cambio', seleccionado: false }
+    { id: 1, roleDescription: 'Agregar comprobantes', status: false },
+    { id: 2, roleDescription: 'Editar comprobantes', status: false },
+    { id: 3, roleDescription: 'Generar reportes', status: false },
+    { id: 4, roleDescription: 'Agregar tasa de cambio', status: false }
   ];
+  // Json para obtener los datos de los empleados
+  data = [
+    {
+      id: 1,
+      fullName: 'Juan Perez',
+      userName: 'juanperez',
+      email: 'juan@gmail.com',
+      imagePath: 'assets/images/avatars/avatar-1.png',
+    },
+    {
+      id: 2,
+      fullName: 'Maria Lopez',
+      userName: 'marialopez',
+      email: 'maria@gmail.com',
+      imagePath: 'assets/images/avatars/avatar-2.png',
+    },
+    {
+      id: 3,
+      fullName: 'Pedro Rodriguez',
+      userName: 'pedrorodriguez',
+      email: 'pedro@gmail.com',
+      imagePath: 'assets/images/avatars/avatar-3.png',
+    },
+    {
+      id: 4,
+      fullName: 'Ana Gomez',
+      userName: 'anagomez',
+      email: 'ana@gmail.com',
+      imagePath: 'assets/images/avatars/avatar-4.png',
+    }
+  ]
+
+  buscarUsuario() {
+    if (this.NombreUsuario) {
+      console.log(this.NombreUsuario);
+      this.userService.getUsersList(this.NombreUsuario).subscribe(
+        (response: any) => {
+          if (response.success) {
+            console.log(response.data);
+            this.resultados = response.data;
+            // this.resultados = this.data.filter((usuario) =>
+            //    usuario.userName.toLowerCase().includes(this.NombreUsuario.toLowerCase())
+            // );
+
+          } else {
+            console.error(response.message);
+          }
+        },
+        (error) => {
+          console.error('Error al obtener lista de usuarios:', error);
+        }
+      );
+      // this.resultados = this.data.filter((usuario) =>
+      //   usuario.fullName.toLowerCase().includes(this.NombreUsuario.toLowerCase())
+      // );
+    } else {
+      this.resultados = [];
+    }
+  }
+
+  seleccionarUsuario(usuario: any) {
+    this.NombreUsuario = usuario.userName;
+    this.resultados = []; // Oculta la lista de sugerencias
+  }
+
   // Logica para el marcado de checkbox
   syncCheckboxes(subsidiary: any, areas: any[]) {
-    if (subsidiary.selected) {
-      areas.forEach((area) => (area.selected = true));
+    if (subsidiary.status) {
+      areas.forEach((area) => (area.status = true));
     } else {
-      areas.forEach((area) => (area.selected = false));
+      areas.forEach((area) => (area.status = false));
     }
   }
   // Guarda listado de id de areas seleccionadas en un array
@@ -85,13 +151,13 @@ export class AddUsersAndPermissionsComponent {
   // Guarda listado de id de roles seleccionadas en un array
   obtenerIdsRolesSeleccionados() {
     const idsSeleccionados = this.roles
-      .filter((rol) => rol.seleccionado)
+      .filter((rol) => rol.status)
       .map((rol) => rol.id);
     console.log('IDs de roles seleccionados:', idsSeleccionados);
   }
   capturarIdsRolesSeleccionados() {
     this.idsRolesSeleccionados = this.roles
-      .filter((rol) => rol.seleccionado)
+      .filter((rol) => rol.status)
       .map((rol) => rol.id);
   }
   // Función para agregar un usuario
