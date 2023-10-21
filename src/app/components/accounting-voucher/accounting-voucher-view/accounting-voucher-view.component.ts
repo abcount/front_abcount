4fe5dc8adacf57d0299cb785224a90de82bdd5af
MicaelaGordillo <micaa.gordilloa@gmail.com>
2023-10-20 20:02:04 -0400

    accounting-voucher-view css

--- src/app/components/accounting-voucher/accounting-voucher-view/accounting-voucher-view.component.ts
+++ src/app/components/accounting-voucher/accounting-voucher-view/accounting-voucher-view.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { TransactionService } from 'src/app/services/transaction.service';
 
 @Component({
   selector: 'app-accounting-voucher-view',
@@ -7,115 +7,108 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./accounting-voucher-view.component.css']
 })
 export class AccountingVoucherViewComponent {
-  hojas: any[] = [this.nuevaHoja()];
-  hojaActual: number = 0;
 
+  // Variables
+  companyName: string = '';
+  sucursales: any[] = [{id: '', name: ''}];
+  areas: any[] = [{id: '', name: ''}];
+  documentos: any[] = [{id: '', name: ''}];
+  fecha: string = '';
+  numComprobante: number = 0;
+  monedas: any[] = [{id: '', name: ''}];
+  glosa: string = '';
+  listTransactionAccount: Row[] = [];
+  totalDebe: number = 0;
+  totalHaber: number = 0;
 
+  constructor(private transactionService: TransactionService) { }
 
-
-  nuevaHoja() {
-    return {
-      entradas: Array(10).fill({}).map(() => ({
-        numeroCuenta: '',
-        nombreCuenta: '',
-        auxiliar: '',
-        entidad: '',
-        debe: 0,
-        haber: 0,
-        glosa: '',
-        cheque: ''
-      }))
-    };
+  // Función inicial
+  ngOnInit(){
+    this.loadVouncherData();
+    this.addEmptyRow();
   }
 
-  get hoja() {
-    return this.hojas[this.hojaActual];
+  // Función para cargar los datos del voucher
+  loadVouncherData(){
+    this.transactionService.getVoucherData().subscribe(response => {
+      const data = response.data;
+      this.companyName = data.companyName;
+      this.sucursales = data.subsidiaries.map((subsidiary: {subsidiaryId: any; subsidiaryName: any;}) => ({id: subsidiary.subsidiaryId, name: subsidiary.subsidiaryName}));
+      this.subsidiarySelect = this.sucursales[0].id;
+      this.areas = data.areas.map((area: {areaId: any; areaName: any;}) => ({id: area.areaId, name: area.areaName}));
+      this.areaSelect = this.areas[0].id;
+      this.documentos = data.transactionType.map((transactionType: { transactionTypeId: any, type: any; }) => ({id: transactionType.transactionTypeId, name: transactionType.type}));
+      this.documentSelect = this.documentos[0].id;
+      this.monedas = data.currencies.map((currency: { exchangeMoneyId: any; moneyName: any; }) => ({id: currency.exchangeMoneyId, name: currency.moneyName}));
+      this.currencySelect = this.monedas[0].id;
+    });
   }
 
-  constructor(private http: HttpClient) {
-    this.cargarTransaccion();
-  }
-
-  agregar() {
-
-  }
-  cargarTransaccion() {
-    const apiUrl = 'http://localhost:8080/voucher';
-
-    this.http.get<any>(apiUrl).subscribe(
-      data => {
-        console.log("Respuesta del servidor:", data);
-        const mappedData = {
-          empresa: '',
-          sucursal: '',
-          fecha: data.date,
-          area: '',
-          numComprobante: data.transaction_number,
-          documento: '',
-          moneda: '',
-          glosa: data.glosa_general,
-          entradas: data.entradas.map((entrada: any) => ({
-            numeroCuenta: '',
-            nombreCuenta: '',
-            auxiliar: entrada.auxiliary_account_id,
-            entidad: entrada.entity_id,
-            debe: 0,
-            haber: 0,
-            glosa: entrada.glosa_detail,
-            cheque: entrada.check
-          }))
+  // Función para agregar una fila vacía
+  addEmptyRow(){
+    const counter = this.listTransactionAccount.length;
+    if (counter < 10) {
+      for(let i = 0; i < 10-counter; i++){
+        const row: Row = {
+          numeroCuenta: '',
+          nombreCuenta: '',
+          auxiliar: '',
+          entidad: '',
+          debe: '',
+          haber: '',
+          glosa: '',
+          nroDoc: '',
+          fechaEmision: ''
         };
-        this.hojas = [mappedData];
-      },
-      error => {
-        console.error('Hubo un error al obtener la transacción:', error);
+        this.listTransactionAccount.push(row);
       }
-    );
+    }
+  }
+
+  // Selects
+  subsidiarySelect: number;
+  areaSelect: number;
+  documentSelect: number;
+  currencySelect: number;
+
+  subsidiarySelected(subsidiary: any) {
+    this.subsidiarySelect = subsidiary.id;
+    //console.log("Sucursal id: "+this.subsidiarySelect);
+  }
+  areaSelected(area: any) {
+    this.areaSelect = area.id;
+    //console.log("Area id: "+this.areaSelect);
+  }
+  documentSelected(document: any) {
+    this.documentSelect = document.id;
+    //console.log("Documento id: "+this.documentSelect);
+  }
+  currencySelected(currency: any) {
+    this.currencySelect = currency.id;
+    //console.log("Moneda id: "+this.currencySelect);
   }
 
 
   navegar(direccion: string) {
     if (direccion === 'derecha') {
-      this.hojaActual++;
-      if (this.hojaActual >= this.hojas.length) {
-        this.hojas.push(this.nuevaHoja());
-      }
-    } else if (direccion === 'izquierda' && this.hojaActual > 0) {
-      this.hojaActual--;
+      
+    } else {
+
     }
+    console.log("Navegar: "+direccion);
   }
-  navegarInputs(event: KeyboardEvent, rowIndex: number, columna: string) {
-    const currentInput = event.target as HTMLElement;
-    let nextInput: HTMLElement | null = null;
 
-    if (!currentInput.parentElement || !currentInput.parentElement.parentElement) {
-      return;
-    }
+}
 
-    const currentRow = currentInput.parentElement.parentElement;
-
-    switch (event.key) {
-      case 'ArrowUp':
-        if (rowIndex > 0 && currentRow.previousElementSibling) {
-          nextInput = currentRow.previousElementSibling.querySelector(`input[name=${columna}]`);
-        }
-        break;
-      case 'ArrowDown':
-        if (rowIndex < this.hoja.entradas.length - 1 && currentRow.nextElementSibling) {
-          nextInput = currentRow.nextElementSibling.querySelector(`input[name=${columna}]`);
-        }
-        break;
-      case 'ArrowLeft':
-        nextInput = currentInput.parentElement.previousElementSibling?.querySelector('input') || null;
-        break;
-      case 'ArrowRight':
-        nextInput = currentInput.parentElement.nextElementSibling?.querySelector('input') || null;
-        break;
-    }
-
-    if (nextInput) {
-      nextInput.focus();
-      event.preventDefault();
-    }
-  }
+interface Row {
+  numeroCuenta: string;
+  nombreCuenta: string;
+  auxiliar: string;
+  entidad: string;
+  debe: string;
+  haber: string;
+  glosa: string;
+  nroDoc: string;
+  fechaEmision: string;
 }
