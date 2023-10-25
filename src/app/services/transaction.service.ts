bacba2611d56c258dd0f41ed779836e4d7edc799
EmersonChipana <emerson.chipana@ucb.edu.bo>
2023-10-25 18:24:26 -0400

    Merge

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -11,14 +11,16 @@ export class TransactionService {
 
 
   constructor(private http: HttpClient) { }
-  companyId = localStorage.getItem('companyId');
+  
 
   getVoucherData(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/${this.companyId}`);
+    const companyId = localStorage.getItem('companyId');
+    return this.http.get(`${this.baseUrl}/${companyId}`);
   }
 
 
   createTransaction(data: any) {
-    return this.http.post(`${this.baseUrl}/${this.companyId}`, data);
+    const companyId = localStorage.getItem('companyId');
+    return this.http.post(`${this.baseUrl}/${companyId}`, data);
   }
 }
