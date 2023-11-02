f69cf71f925fc8712f5ff65e8ff10daf8dfbf80f
MicaelaGordillo <micaa.gordilloa@gmail.com>
2023-11-01 22:45:50 -0400

    Remove chart of accounts from balance sheet

--- src/app/components/reports/balance-sheet/balance-sheet-form/balance-sheet.component.ts
+++ src/app/components/reports/balance-sheet/balance-sheet-form/balance-sheet.component.ts
@@ -19,7 +19,6 @@ export class BalanceSheetComponent {
   @Input() currencies: any[] = [];
   @Input() principalCurrency: any = '';
   @Input() otherCurrencySelected: string = '0';
-  @Input() accountPlan: any[] = [];
   @Output() flagChange = new EventEmitter<boolean>();
 
   closeModal() {
@@ -30,7 +29,6 @@ export class BalanceSheetComponent {
 
   dateTo: string = '';
   currencySelected: string = '0';
-  accountsChecked: any[] = [];
   name: string[] = [];
   @ViewChild('errorMessage') errorMessage: ElementRef;
   errorMessageText: string = 'Por favor, seleccione al menos una sucursal.';
@@ -40,37 +38,27 @@ export class BalanceSheetComponent {
     if (sucursalesId.length > 0) {
       const areasId = this.areas.filter(area => area.isChecked).map(area => area.areaId);
       if (areasId.length > 0) {
-        this.accountsChecked = [];
-        this.getAccountsPlanChecked(this.accountPlan);
-        //console.log(this.accountsChecked);
-        const accountsId = this.accountsChecked.map(account => account.accountId);
-        if (this.accountsChecked.length>0) {
-          if (this.dateTo != '') {
-            var currencyId = '0';
-            if (this.currencySelected == '0') {
-              currencyId = this.principalCurrency.exchangeMoneyId;
-            } else {
-              currencyId = this.otherCurrencySelected;
-            }
-            const names = this.name.filter((name: string) => {
-              const nameSpace = name.trim();
-              return nameSpace.length > 0;
-            });
-            const data = {
-              subsidiaries: sucursalesId,
-              areas: areasId,
-              to: this.dateTo,
-              accountsId: accountsId,
-              currency: currencyId,
-              responsible: names
-            }
-            console.log(data);
+        if (this.dateTo != '') {
+          var currencyId = '0';
+          if (this.currencySelected == '0') {
+            currencyId = this.principalCurrency.exchangeMoneyId;
           } else {
-            this.errorMessageText = 'Por favor, seleccione una fecha.';
-            this.showErrorMessage();
+            currencyId = this.otherCurrencySelected;
           }
+          const names = this.name.filter((name: string) => {
+            const nameSpace = name.trim();
+            return nameSpace.length > 0;
+          });
+          const data = {
+            subsidiaries: sucursalesId,
+            areas: areasId,
+            to: this.dateTo,
+            currency: currencyId,
+            responsible: names
+          }
+          console.log(data);
         } else {
-          this.errorMessageText = 'Por favor, seleccione al menos una cuenta.';
+          this.errorMessageText = 'Por favor, seleccione una fecha.';
           this.showErrorMessage();
         }
       } else {
@@ -85,17 +73,6 @@ export class BalanceSheetComponent {
 
   generateExcel(){
 
-  }
-
-  getAccountsPlanChecked(accounts: any[]) {
-    for(let i = 0; i < accounts.length; i++){
-      if(accounts[i].isChecked){
-        this.accountsChecked.push(accounts[i]);
-      }
-      if(accounts[i].childrenAccounts.length > 0){
-        this.getAccountsPlanChecked(accounts[i].childrenAccounts);
-      }
-    }
   }
 
   showErrorMessage() {
